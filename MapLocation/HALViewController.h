69654ae7c35c406476841d5137ebd1709a6a43dd
hanssenli <hanssenli@gmail.com>
2013-02-15 11:34:14 -0500

    Initial Commit

---
+++ MapLocation/HALViewController.h
@@ -0,0 +1,13 @@
+//
+//  HALViewController.h
+//  MapLocation
+//
+//  Created by Hanssen Li on 2/15/13.
+//  Copyright (c) 2013 Hanssen Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HALViewController : UIViewController
+
+@end
